5973739e755aca58a68e245d1549bba32bdffc81
Manuel Guerrero <manu_pacu@hotmail.com>
2022-10-22 22:45:12 -0500

    login action and page redirect

--- src/app/pages/login.tsx
+++ src/app/pages/login.tsx
@@ -1,20 +1,76 @@
-import { connect } from "react-redux";
-import React, { Component } from "react";
+import { useDispatch } from "react-redux";
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { decode as base64_decode, encode as base64_encode } from "base-64";
+import { useNavigate } from "react-router-dom";
+import { login } from "../../features/auth/authSlice";
 
 type Props = {};
 
-type State = {};
+function Login({}: Props) {
+  const [user, setUser] = useState("");
+  const [password, setPassword] = useState("");
 
-export class login extends Component<Props, State> {
-  state = {};
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-  render() {
-    return <div>login</div>;
-  }
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+
+    const token = base64_encode(`${user}:${password}`);
+
+    await dispatch(login(token));
+    return navigate("/employees");
+  };
+
+  return (
+    <div className="container p-2">
+      <div className="d-flex flex-column justify-content-center align-items-center">
+        <Form onSubmit={handleSubmit}>
+          <Form.Group className="mb-3" controlId="formUser">
+            <Form.Label>User</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Jane Doe"
+              onPaste={(e: any) => {
+                e.preventDefault();
+                return false;
+              }}
+              onCopy={(e: any) => {
+                e.preventDefault();
+                return false;
+              }}
+              value={user}
+              onChange={(user) => setUser(user.target.value)}
+            />
+            <Form.Text className="text-muted">Enter your Username</Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              value={password}
+              onPaste={(e: any) => {
+                e.preventDefault();
+                return false;
+              }}
+              onCopy={(e: any) => {
+                e.preventDefault();
+                return false;
+              }}
+              onChange={(password) => setPassword(password.target.value)}
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
+    </div>
+  );
 }
 
-const mapStateToProps = () => ({});
-
-const mapDispatchToProps = {};
-
-export default connect(mapStateToProps, mapDispatchToProps)(login);
+export default Login;
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
-import { HashRouter } from "react-router-dom";
 import App from "./app/containers/app";
 import reportWebVitals from "./reportWebVitals";
 import "./index.css";
